3ae02beb9832fe2d15fb4d2072546a54818bdc15
Antonio <antonio.mario.demarco@gmail.com>
2023-10-11 16:56:06 +0200

    Primo Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+    public class Main{
+        public static void main (String[] args){
+            double base = 5;
+            double altezza = 4;
+            double area = base*altezza;
+            System.out.println(area);
+        }
+    }
