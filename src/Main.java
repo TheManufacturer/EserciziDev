a215d5bee80b994255a2dcfa445abf7b33cc28f4
Antonio <antonio.mario.demarco@gmail.com>
2023-10-12 17:30:03 +0200

    Risolto Area

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
         public static void main (String[] args){
             double base = 5;
             double altezza = 4;
-            double area = base*altezza;
+            double area = (base*altezza)/2;
             System.out.println(area);
         }
     }
