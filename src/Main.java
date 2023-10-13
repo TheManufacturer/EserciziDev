3a42aaa0a8eb65a76957b4dd6759892069e8003a
Antonio <antonio.mario.demarco@gmail.com>
2023-10-13 15:39:28 +0200

    Esercizio 19 PerdoTempoNegliEsercizEnonDovrei

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int limite = 5;
-        int somma=0;
-        System.out.println(stampaSomma(limite));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("dammi il valore");
+        int valore= scanner.nextInt();
+        esegMolt(valore);
+
     }
 
-    public static int stampaSomma(int lim) {
-        int max=1;
-        int som=0;
-        while(max<lim){
-            som=som+max;
-            max++;
+    public static void esegMolt(int val) {
+        int molt;
+        for(int i=1; i<=10; i++){
+            molt=val * i;
+            System.out.println(val + " x " + i + "= " + molt);
         }
-        return som;
     }
+
 }
 
 /*
-Scrivere un programma che contenga un metodo che sfrutti il while loop
-e prenda in ingresso 1 valore intero come limite superiore e
-calcoli la somma di tutti i valori prima del limite e la stampi a video.
-(Esempio passo 5 come limite e otterrò la somma di 1 + 2 + 3 + 4)
-*/
+Scrivi un programma che contenga
+un metodo che prende in ingresso un valore numerico
+e restituisca la tabellina aritmetica di quel numero
+che dovrà essere stampata a video.
+
+#SonoLentissimooo XD
+ */
