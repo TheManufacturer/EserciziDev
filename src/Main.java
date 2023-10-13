e9f987fe46bc7758ab3f84df0b021295d882f831
Antonio <antonio.mario.demarco@gmail.com>
2023-10-13 12:58:10 +0200

    Esercizio 16 commit Operation

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,32 @@
 public class Main {
     public static void main(String args[]) {
-        int number = 0;
-        numberForDay(number);
+        char mathOperation = 'a';
+        numberForDay(mathOperation);
     }
 
-    public static void numberForDay(int n) {
-        switch (n){
-            case 1:
-                System.out.println("Lunedì");
+    public static void numberForDay(char oper) {
+        switch (oper){
+            case 'a':
+                System.out.println("Addizione");
                 break;
-            case 2:
-                System.out.println("Martedì");
+            case 's':
+                System.out.println("Sottrazione");
                 break;
-            case 3:
-                System.out.println("Mercoledì");
+            case 'm':
+                System.out.println("Moltiplicazione");
                 break;
-            case 4:
-                System.out.println("Giovedì");
-                break;
-            case 5:
-                System.out.println("Venerdì");
-                break;
-            case 6:
-                System.out.println("Sabato");
-                break;
-            case 7:
-                System.out.println("Domenica");
+            case 'd':
+                System.out.println("Divisione");
                 break;
             default:
-                System.out.println("Sei sicuro di aver inserito un valore corretto? [1-7]");
+                System.out.println("Sei sicuro di aver inserito un valore corretto? [a-s-m-d]");
         }
     }
 
 }
 
 /*
-Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico
-e restituisca il nome del giorno della settimana come stringa o una stringa di errore
-nel caso di valore maggiore di 7 o minore di 1.
+Scrivi un programma che contenga un metodo che prende in ingresso un carattere
+e ne identifica il tipo per un operazione di algebra (+ addizione - sottrazione * moltiplicazione / divisione).
+Nel caso non riesca ad identificare il tipo di operazione dovrà restituire una stringa di errore.
 */
