3dab546792df4cca0578dd50d71a2d77385859f9
Antonio <antonio.mario.demarco@gmail.com>
2023-10-14 19:32:40 +0200

    Esercizio 27 MetodoScambioRC

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,54 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix =
                 {
-                        {1, 2, 3, 4, 5},
-                        {6, 7, 8, 9, 10},
-                        {11, 12, 13, 14, 15}
+                        {1, 2, 3},
+                        {4, 5, 6}
                 };
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int sommaPrimaRiga = 0;
 
-        for (int riga = 0; riga < n; riga++) {
-            for (int colonna = 0; colonna < m; colonna++) {
-                System.out.print(matrix[riga][colonna] + " ");
-                sommaPrimaRiga=+matrix[riga][colonna];
+        int n = matrix.length;      // Indice delle righe
+        int m = matrix[0].length;   // Indice delle colonne
+
+        int[][] newMatrix = scambioRC(matrix, n, m);
+        stampaMatrice(matrix);
+        stampaMatrice(newMatrix);
+    }
+
+    public static int[][] scambioRC(int[][] matrice, int righe, int colonne) {
+        int[][] nuovaMatrice = new int[colonne][righe];
+
+        for (int riga = 0; riga < righe; riga++) {
+            for (int colonna = 0; colonna < colonne; colonna++) {
+                nuovaMatrice[colonna][riga] = matrice[riga][colonna];
+            }
+        }
+
+        return nuovaMatrice;
+    }
+
+    public static void stampaMatrice(int[][] matrice) {
+        for (int riga = 0; riga < matrice.length; riga++) {
+            for (int colonna = 0; colonna < matrice[0].length; colonna++) {
+                System.out.print(matrice[riga][colonna] + " ");
             }
             System.out.println();
         }
-        System.out.println("La somma della prima riga è --> " + sommaPrimaRiga);
     }
 }
 
+
 /*
-Scrivere un programma che contenga un metodo che:
-    permette di inizializzare una matrice riempita con dei valori a vostro piacimento
-        restituisca la somma degli elementi sulla prima riga.
-    ------
-        int n= array.length;        --> Lunghezza righe
-        int m= array[0].length;     --> Lunghezza colonne
+Scrivere un programma che contenga un metodo che
+permette di inizializzare una matrice e di scambiare
+le sue righe e le sue colonne stampandola a video.
+
+Matrice di partenza
+
+1 2 3
+4 5 6
+Matrice risultato
+
+ 1 4
+ 2 5
+ 3 6
  */
 
