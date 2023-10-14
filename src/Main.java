3d1144835461d6b6bb73504140c83287f63e6008
Antonio <antonio.mario.demarco@gmail.com>
2023-10-14 12:51:53 +0200

    Esercizio 24 Uso_Random -Modificato

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,37 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        int contatoreA=0;
-        char[] vector = new char[10];
-        Random random = new Random();
+        char[] charArray = createAndFillArray(10); // Crea un array di 20 caratteri casuali
+        printArrayOccurrences(charArray, 'a');     // Stampa le occorrenze del carattere 'a'
+    }
 
-        // Riempire il vettore con caratteri casuali
-        for (int i = 0; i < 10; i++) {
-            char randomChar = (char) (random.nextInt(26) + 'a'); // Genera una lettera minuscola casuale
-            if(randomChar!='a') {
-                vector[i] = randomChar;
-            }else{
-                vector[i] = randomChar;
-                contatoreA++;
+    // Metodo per creare e riempire un array di caratteri casuali
+    public static char[] createAndFillArray(int length) {
+        char[] array = new char[length]; // Crea un nuovo array di caratteri con la lunghezza specificata
+        for (int i = 0; i < length; i++) {
+            char randomChar = (char) ('a' + (int) (Math.random() * 26)); // Genera caratteri casuali tra 'a' e 'z'
+            array[i] = randomChar; // Assegna il carattere casuale all'array
+            System.out.println(array[i]);
+        }
+        return array; // Restituisce l'array riempito
+    }
+
+    // Metodo per contare e stampare le occorrenze di un carattere in un array
+    public static void printArrayOccurrences(char[] array, char target) {
+        int count = 0; // Inizializza il contatore delle occorrenze a zero
+
+        for (char c : array) {
+            if (c == target) {
+                count++; // Incrementa il contatore se il carattere corrente è uguale al carattere target
             }
         }
 
-        System.out.println("Riscontrato char < a > -->" + contatoreA);
-
-
-        // Stampa il vettore
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Elemento " + (i+1) + ": " + vector[i]);
+        if (count > 0) {
+            System.out.println("Occorrenze del carattere '" + target + "': " + count); // Stampa il numero di occorrenze
+        } else {
+            System.out.println("Nessuna occorrenza del carattere '" + target + "'"); // Stampa un messaggio se non ci sono occorrenze
         }
-        System.out.println(contatoreA);
     }
 }
-
 
 
 /*
