d3e68d30b5837751a9c141f2a4ca6c5235f9efcf
Antonio <antonio.mario.demarco@gmail.com>
2023-10-14 19:02:16 +0200

    Esercizio 26 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,32 @@
 public class Main {
-        public static void main(String[] args) {
-            int[] array = {1, 2, 3, 2, 4, 3, 5, 6, 6, 7}; // Array con elementi duplicati
-            int[] clone = trovaeDistruggi(array); // Chiama il metodo per rimuovere duplicati
+    public static void main(String[] args) {
+        int[][] matrix =
+                {
+                        {1, 2, 3, 4, 5},
+                        {6, 7, 8, 9, 10},
+                        {11, 12, 13, 14, 15}
+                };
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int sommaPrimaRiga = 0;
 
-            System.out.println("Array con duplicati: ");    // Stampa l'array risultante senza duplicati
-            for (int i = 0; i < array.length; i++) {
-                System.out.print("a. " +array[i]);
+        for (int riga = 0; riga < n; riga++) {
+            for (int colonna = 0; colonna < m; colonna++) {
+                System.out.print(matrix[riga][colonna] + " ");
+                sommaPrimaRiga=+matrix[riga][colonna];
             }
             System.out.println();
-
-            System.out.println("Array senza duplicati: ");    // Stampa l'array risultante senza duplicati
-            for (int i = 0; i < clone.length; i++) {
-                System.out.print("b." + " " +clone[i]);
-            }
         }
-
-        // Metodo per rimuovere gli elementi duplicati da un array di interi
-        public static int[] trovaeDistruggi(int[] arr) {
-            int n = arr.length; // Lunghezza dell'array originale
-
-            // Conta quanti elementi unici ci sono nell'array
-            int contUnici = 0;
-            for (int i = 0; i < n; i++) {
-                boolean isDuplicate = false;
-                for (int j = 0; j < i; j++) {
-                    if (arr[i] == arr[j]) {
-                        isDuplicate = true;
-                        break;
-                    }
-                }
-                if (!isDuplicate) {
-                    contUnici++;
-                }
-            }
-
-            // Crea un nuovo array per gli elementi unici
-            int[] cleanArray = new int[contUnici];
-            int index = 0;
-            for (int i = 0; i < n; i++) {
-                boolean isDuplicate = false;
-                for (int j = 0; j < i; j++) {
-                    if (arr[i] == arr[j]) {
-                        isDuplicate = true;
-                        break;
-                    }
-                }
-                if (!isDuplicate) {
-                    cleanArray[index] = arr[i];
-                    index++;
-                }
-            }
-
-            return cleanArray; // Restituisce l'array senza duplicati
-        }
+        System.out.println("La somma della prima riga è --> " + sommaPrimaRiga);
     }
+}
 
 /*
 Scrivere un programma che contenga un metodo che:
-
-Permetta di rimuovere gli elementi duplicati
-in un array di interi.
-
-L'array ottenuto dovrà essere stampato a video.
-
--------
-Andiamo ad utilizzare questi 2 for (i / j) per -->
-**      confrontare la posizione attuale con la precedente
-
-*   Al primo ciclo del for esterno non viene eseguito il for interno    *
-_______
-for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] == arr[j]) {
-                    break;
-                }
-            }
- _______
- -------
+    permette di inizializzare una matrice riempita con dei valori a vostro piacimento
+        restituisca la somma degli elementi sulla prima riga.
+    ------
+        int n= array.length;        --> Lunghezza righe
+        int m= array[0].length;     --> Lunghezza colonne
  */
 
--- src/TrovAeDistruggi.java
+++ src/TrovAeDistruggi.java
@@ -1,4 +1,4 @@
-public class ExternProjectClass {
+public class TrovAeDistruggi {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 2, 4, 3, 5, 6, 6, 7}; // Array con elementi duplicati
         int[] clone = trovaeDistruggi(array); // Chiama il metodo per rimuovere duplicati
