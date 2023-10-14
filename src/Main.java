e977eea5b67fc33bc7dc12671312eba100d54058
Antonio <antonio.mario.demarco@gmail.com>
2023-10-14 18:15:36 +0200

    Esercizio 25 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,81 @@
 public class Main {
-    public static void main(String[] args) {
-        char[] charArray = createAndFillArray(10); // Crea un array di 20 caratteri casuali
-        printArrayOccurrences(charArray, 'a');     // Stampa le occorrenze del carattere 'a'
-    }
+        public static void main(String[] args) {
+            int[] array = {1, 2, 3, 2, 4, 3, 5, 6, 6, 7}; // Array con elementi duplicati
+            int[] clone = trovaeDistruggi(array); // Chiama il metodo per rimuovere duplicati
 
-    // Metodo per creare e riempire un array di caratteri casuali
-    public static char[] createAndFillArray(int length) {
-        char[] array = new char[length]; // Crea un nuovo array di caratteri con la lunghezza specificata
-        for (int i = 0; i < length; i++) {
-            char randomChar = (char) ('a' + (int) (Math.random() * 26)); // Genera caratteri casuali tra 'a' e 'z'
-            array[i] = randomChar; // Assegna il carattere casuale all'array
-            System.out.println(array[i]);
-        }
-        return array; // Restituisce l'array riempito
-    }
+            System.out.println("Array con duplicati: ");    // Stampa l'array risultante senza duplicati
+            for (int i = 0; i < array.length; i++) {
+                System.out.print("a. " +array[i]);
+            }
+            System.out.println();
 
-    // Metodo per contare e stampare le occorrenze di un carattere in un array
-    public static void printArrayOccurrences(char[] array, char target) {
-        int count = 0; // Inizializza il contatore delle occorrenze a zero
-
-        for (char c : array) {
-            if (c == target) {
-                count++; // Incrementa il contatore se il carattere corrente è uguale al carattere target
+            System.out.println("Array senza duplicati: ");    // Stampa l'array risultante senza duplicati
+            for (int i = 0; i < clone.length; i++) {
+                System.out.print("b." + " " +clone[i]);
             }
         }
 
-        if (count > 0) {
-            System.out.println("Occorrenze del carattere '" + target + "': " + count); // Stampa il numero di occorrenze
-        } else {
-            System.out.println("Nessuna occorrenza del carattere '" + target + "'"); // Stampa un messaggio se non ci sono occorrenze
+        // Metodo per rimuovere gli elementi duplicati da un array di interi
+        public static int[] trovaeDistruggi(int[] arr) {
+            int n = arr.length; // Lunghezza dell'array originale
+
+            // Conta quanti elementi unici ci sono nell'array
+            int contUnici = 0;
+            for (int i = 0; i < n; i++) {
+                boolean isDuplicate = false;
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] == arr[j]) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    contUnici++;
+                }
+            }
+
+            // Crea un nuovo array per gli elementi unici
+            int[] cleanArray = new int[contUnici];
+            int index = 0;
+            for (int i = 0; i < n; i++) {
+                boolean isDuplicate = false;
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] == arr[j]) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    cleanArray[index] = arr[i];
+                    index++;
+                }
+            }
+
+            return cleanArray; // Restituisce l'array senza duplicati
         }
     }
-}
-
 
 /*
-Scrivi un programma che contenga un metodo che crea un array
-lo riempie con dei caratteri.
-Questo metodo dovrà poi stampare tutte le occorrenze del carattere
- -->  'a' oppure--> 0 [in caso non ce ne siano].
+Scrivere un programma che contenga un metodo che:
+
+Permetta di rimuovere gli elementi duplicati
+in un array di interi.
+
+L'array ottenuto dovrà essere stampato a video.
+
+-------
+Andiamo ad utilizzare questi 2 for (i / j) per -->
+**      confrontare la posizione attuale con la precedente
+
+*   Al primo ciclo del for esterno non viene eseguito il for interno    *
+_______
+for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    break;
+                }
+            }
+ _______
+ -------
  */
+
